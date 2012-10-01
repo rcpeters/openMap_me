cc04c0734a5c707b6f5378dfc99d71957a777bed
rcpeters <info@rcpeters.com>
2012-10-01 11:14:50 -0700

    fix url structure to static pages

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,33 @@ var mapProvider = require('../project_modules/data_providers/mapProvider').MapPr
 var cookieId = 'oMapV3';
  
 exports.index = function(req, res){
-	res.render('index', {});
-		//res.render('index', { title: 'Open Map Me', 'map': map, 'users': users, 'cookieId': cookieId});
+	console.log(req.url);
+	var mapId = "";
+	if (req.url.length > 4) mapId = req.url.substring(5).toUpperCase();
+	else if (req.cookies[cookieId]) {  
+		var cookieJson =  JSON.parse(req.cookies[cookieId]);
+		res.redirect('/map_' +  cookieJson.curMapId);
+		return;
+	}
+	console.log("mapId mapId mapId mapId mapId mapId " + mapId);
+	
+	if (mapId == undefined || mapId == '') {
+		mapProvider.createMap(function (error, map) {
+			res.redirect('/map_' + map.id);
+		});
+	} else {  
+			mapProvider.getMap(mapId, function(error, map) {
+			if (error) {
+				mapProvider.createMap(function (error, newMap) {
+					console.log("bad request sent to new map" + newMap.id);
+					res.redirect('/map_' + newMap.id);
+				});
+			} else {
+				mapProvider.getUsers(map.id, function (users) {
+					console.log(users);
+					res.render('index', {});
+				});
+			}
+		});
+	}
 };
